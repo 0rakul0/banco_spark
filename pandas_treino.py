087fc8815df04a4cdd16701795824c2f33c4037a
ANJOS, J. S <0rakul0render@gmail.com>
2023-03-07 22:46:28 -0300

    base

--- pandas_treino.py
+++ pandas_treino.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import spacy
+import numpy as np
 import random
 from spacy.util import minibatch, compounding
-from spacy.gold import GoldParse
 import seaborn as sns
 import matplotlib.pyplot as plt
-from spacy.lang.pt.stop_words import STOP_WORDS
+from util.stop_words import STOP_WORDS
 
 
 def load_data(file_path, text_col, label_col, label_map=None):
@@ -23,7 +23,7 @@ def load_data(file_path, text_col, label_col, label_map=None):
     return [(text, label) for text, label in zip(df[text_col], df[label_col])]
 
 
-def train_textcat_model(train_data, model_path, n_iter=10, dropout=0.2, batch_size=16, lemmatize=False, remove_stopwords=False):
+def train_textcat_model(train_data, model_path, n_iter=10, dropout=0.2, batch_size=16, lemmatize=True, remove_stopwords=True):
     """
     Treina um modelo de classificação de texto do spaCy, salva-o em um arquivo e retorna o caminho do arquivo.
     :param train_data: lista de tuplas (texto, label) de treinamento
@@ -35,9 +35,10 @@ def train_textcat_model(train_data, model_path, n_iter=10, dropout=0.2, batch_si
     :param remove_stopwords: se True, remove as stop words dos textos
     :return: caminho do arquivo onde o modelo foi salvo
     """
+    global golds
     nlp = spacy.load('pt_core_news_lg')
-    nlp.vocab.add_flag(lambda s: s.lower() in STOP_WORDS, spacy.attrs.IS_STOP)
-    nlp = spacy.blank('pt')
+    if remove_stopwords:
+        nlp.vocab.add_flag(lambda s: s.lower() in STOP_WORDS, spacy.attrs.IS_STOP)
     if 'textcat' not in nlp.pipe_names:
         textcat = nlp.create_pipe('textcat')
         nlp.add_pipe(textcat, last=True)
@@ -54,19 +55,27 @@ def train_textcat_model(train_data, model_path, n_iter=10, dropout=0.2, batch_si
         losses = {}
         for batch in batches:
             texts, labels = zip(*batch)
-            # Aplica a tokenização, stemming e remove as stop words
-            docs = [nlp(text.lower()) for text in texts]
-            stemmed_docs = []
-            for doc in docs:
+            # Aplica a tokenização e lematização (se necessário)
+            docs = []
+            for text in texts:
+                doc = nlp(text.lower())
+
+                # remove as stop_words
+                if remove_stopwords:
+                    doc = [token for token in doc if not token.is_stop]
+
+                # deixa o radical da frase
                 if lemmatize:
-                    stemmed_docs.append(" ".join([token.lemma_ for token in doc if not token.is_stop]))
+                    doc = [token.lemma_ for token in doc]
                 else:
-                    stemmed_docs.append(" ".join([token.text for token in doc if not token.is_stop]))
-            if remove_stopwords:
-                golds = [{"cats": {label: True}} for label in labels]
-            else:
-                golds = [{"cats": {label: True}} for label in labels]
-            nlp.update(stemmed_docs, golds, sgd=optimizer, drop=dropout, losses=losses)
+                    doc = [token.text for token in doc]
+
+                # junta os textos
+                docs.append(" ".join(doc))
+
+            golds = [{"cats": {label: True}} for label in labels]
+
+            nlp.update(docs, golds, sgd=optimizer, drop=dropout, losses=losses)
 
     # Salva o modelo
     nlp.to_disk(model_path)
@@ -74,20 +83,24 @@ def train_textcat_model(train_data, model_path, n_iter=10, dropout=0.2, batch_si
 
 
 # Define a função para gerar o mapa de calor
-def plot_textcat_heatmap(doc):
-    global token
-    labels = [label for label in doc.cats.keys()]
+def plot_textcat_heatmap(doc, decisao):
+    doc = [token for token in doc if not token.is_stop]
     data = []
     for token in doc:
         row = [token.text] + [token.vector[i] for i in range(len(token.vector))]
         data.append(row)
     df = pd.DataFrame(data, columns=['token'] + [f'vec_{i}' for i in range(len(token.vector))])
-    df = pd.melt(df, id_vars=['token'], value_vars=[f'vec_{i}' for i in range(len(token.vector))], var_name='dim', value_name='value')
+    df = pd.melt(df, id_vars=['token'], value_vars=[f'vec_{i}' for i in range(len(token.vector))], var_name='dim',
+                 value_name='value')
     df['abs_value'] = df['value'].abs()
     pivot_table = df.pivot_table(index='token', columns='dim', values='abs_value', aggfunc='max')
+    pivot_table = pivot_table.loc[:, (pivot_table.mean() > pivot_table.mean().mean())]
+    pivot_table_grouped = pivot_table.groupby(np.arange(len(pivot_table.columns)) // 10, axis=1).max()
     if pivot_table.size > 0:
-        sns.heatmap(pivot_table, cmap='Reds', xticklabels=True, yticklabels=True)
-        plt.show()
+        ax = sns.heatmap(pivot_table_grouped, cmap='Reds', xticklabels=True, yticklabels=True, )
+        ax.set_title = decisao
+        file_name = str(doc).replace(' ','_').replace('.','').replace(',', '').replace('[','').replace(']','')
+        plt.savefig(f'./img/{file_name}_{decisao}.png')
     else:
         print("A matriz de frequência está vazia.")
 
@@ -98,7 +111,7 @@ text_col = 'texto_sentenca'
 label_col = 'sentenca'
 label_map = {'label_original': 'nova_label'}
 train_data = load_data(file_path, text_col, label_col, label_map=label_map)
-model_path = "modelo_textcat"
+model_path = "./modelo_textcat"
 
 # Treina o modelo e salva-o em um arquivo
 n_iter = 10
@@ -110,10 +123,15 @@ train_textcat_model(train_data, model_path, n_iter=n_iter, dropout=dropout, batc
 nlp = spacy.load(model_path)
 
 # Testa o modelo
-text = "O réu nega ter tido qualquer envolvimento com o crime."
+text = "O réu admitiu ter cometido o crime."
 doc = nlp(text)
-plot_textcat_heatmap(doc)
+score = doc.cats
 if doc.cats['Procedente'] > doc.cats['Improcedente']:
+    decisao = 'procedente'
     print(f"{text} é procedente")
 else:
+    decisao = 'improcedente'
     print(f"{text} é improcedente")
+
+plot_textcat_heatmap(doc, decisao)
+plt.show()
---
+++ util/stop_words.py
@@ -0,0 +1,72 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+
+STOP_WORDS = set(
+    """
+à às área acerca ademais adeus agora ainda algo algumas alguns ali além ambas ambos antes
+ao aos apenas apoia apoio apontar após aquela aquelas aquele aqueles aqui aquilo
+as assim através atrás até aí a
+
+baixo bastante bem boa bom breve ba be bi bo bu
+
+cada caminho catorze cedo cento certamente certeza cima cinco coisa com como
+comprida comprido conhecida conhecido conselho contra contudo corrente cuja
+cujo custa cá ca ce ci co cu
+
+da daquela daquele dar das de debaixo demais dentro depois des desde dessa desse
+desta deste deve devem deverá dez dezanove dezasseis dezassete dezoito diante
+direita disso diz dizem dizer do dois dos doze duas dá dão du di
+
+é és ela elas ele eles em embora enquanto entre então era essa essas esse esses esta
+estado estar estará estas estava este estes esteve estive estivemos estiveram
+estiveste estivestes estou está estás estão eu eventual exemplo e
+
+falta fará favor faz fazeis fazem fazemos fazer fazes fazia faço fez fim final
+foi fomos for fora foram forma foste fostes fui fa fi fo fu
+
+geral grande grandes grupo ga go gu
+
+inclusive iniciar inicio ir irá isso isto i
+
+já je ji 
+
+lado lhe ligado local logo longe lugar lá le lo lu
+
+maior maioria maiorias mais mal mas me meio menor menos meses mesmo meu meus mil
+minha minhas momento muito muitos máximo mês ma me mi mo mu
+
+na nada naquela naquele nas nem nenhuma nessa nesse nesta neste no nos nossa
+nossas nosso nossos nova novas nove novo novos num numa nunca nuns não nível nós
+número números na ne ni no nu
+
+obrigada obrigado oitava oitavo oito onde ontem onze ora os ou outra outras outros o
+
+para parece parte partir pegar pela pelas pelo pelos perto pode podem poder poderá
+podia pois ponto pontos por porquanto porque porquê portanto porém posição
+possivelmente posso possível pouca pouco povo primeira primeiro próprio próxima
+próximo puderam pôde põe põem
+
+quais qual qualquer quando quanto quarta quarto quatro que quem quer querem quero
+questão quieta quieto quinta quinto quinze quê
+
+relação
+
+sabe saber se segunda segundo sei seis sem sempre ser seria sete seu seus sexta
+sexto sim sistema sob sobre sois somente somos sou sua suas são sétima sétimo só
+
+tais tal talvez também tanta tanto tarde te tem temos tempo tendes tenho tens
+tentar tentaram tente tentei ter terceira terceiro teu teus teve tipo tive
+tivemos tiveram tiveste tivestes toda todas todo todos treze três tu tua tuas
+tudo tão têm
+
+um uma umas uns usa usar último u
+
+vai vais valor veja vem vens ver vez vezes vinda vindo vinte você vocês vos vossa
+vossas vosso vossos vários vão vêm vós
+
+zero 
+
+.
+""".split()
+)
